6fe05055327013972d1151dc5e84b0f51febaee9
sanrree <shavibimi@gmail.com>
2022-07-11 22:01:44 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { INCREMENT } from './redux';
 
 function App() {
+  const { content } = useSelector(x => x)
+  const dispatch = useDispatch()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>side bar</div>
     </div>
   );
 }
